13d0adb183fcaa7a2f0f0d3192a89af114292bf5
darshanmarathe <darshanmarathe@gmail.com>
2026-02-21 19:28:00 +0530

    made install file backup

---
+++ install.bat
@@ -0,0 +1,26 @@
+cls
+del ".install.bat.un~"
+del "installe.bat~"
+call start %TEMP%
+pause
+call settime
+pause
+nvm install node --reinstall-packages-from=node
+pause
+nvm use node
+pause
+choco list
+pause
+choco outdated
+pause
+choco upgrade GoogleChrome
+pause
+choco upgrade all -v --ignore-checksums
+pause
+call npm list -g --depth=0
+pause
+call npm outdated -g --depth=0
+pause
+call npm update -g
+pause 
+nvm list
